7248ee1a08e1b5c97de47e741dd5ebf5b85b8024
Neha Ambewadkar <nehaambewadkar396@gmail.com>
2026-08-23 23:07:14 +0530

    Add Merge Strings Alternately solution

---
+++ 01-Merge-Strings-Alternately/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        
+        string result="";
+       for (int i = 0; i < max(word1.size(), word2.size()); i++)
+       {
+        if( i<word1.size())
+        {
+            result=result+word1[i];
+        }
+        if(i<word2.size())
+        {
+            result=result+word2[i];
+        }
+
+       }
+        return result;
+    }
+    
+};
